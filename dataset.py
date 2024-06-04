6ae365bdbc7976aa92facbbfeebd44560c67dadf
tzu901 <c2221a5335@gmail.com>
2024-06-05 07:50:10 +0800

    0605 07:50

---
+++ dataset.py
@@ -0,0 +1,53 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+
+class SatelliteDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx])
+
+        print(f"Image path: {img_path}")
+        print(f"Mask path: {mask_path}")
+
+        image = cv2.imread(img_path)
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.transform:
+            augmented = self.transform(image=image, mask=mask)
+            image = augmented['image']
+            mask = augmented['mask']
+        
+        return image, mask
+    
+
+if __name__ == "__main__":
+    dataset = SatelliteDataset(
+        image_dir = "./src/data/buildings/train_images/imgs/", 
+        mask_dir = "./src/data/buildings/train_masks/masks/"
+        )
+    
+    print(f"Dataset size: {dataset.__len__()}")
+
+    image, mask = dataset.__getitem__(0)
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    ax[0].imshow(image)
+    ax[0].set_title('Image')
+    ax[1].imshow(mask, cmap='gray')
+    ax[1].set_title('Mask')
+    plt.show()
---
+++ main.py
@@ -0,0 +1,2 @@
+from dataset import SatelliteDataset
+import torch
